cb312a7d758707ef06fed6bea728947b4f6517de
MENNO.BRANDT <brandt.menno@gmail.com>
2020-10-05 22:48:27 +1030

    Homescreen layout

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
@@ -11,7 +12,7 @@ class MyGrid(Widget):
     pass
 
 
-class MyApp(App):
+class MyApp(App): # <- Main Class
     def build(self):
         return MyGrid()
 
