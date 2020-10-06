0a98b141a65d10c0ce7dee13d1508f389e4de7d3
mennobrandt <menno.brandt@tcc.sa.edu.au>
2020-10-06 18:38:19 +1030

    Screen manager for changing pages

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.uix.image import  Image
+from kivy.uix.gridlayout import GridLayout # wasd
+from kivy.uix.widget import Widget # wasd
+from kivy.lang import Builder 
+from kivy.uix.screenmanager import ScreenManager, Screen 
 
 
-class MyGrid(Widget):
+
+
+class MainWindow(Screen):
     pass
 
+class SecondWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("my.kv")
 
 class MyApp(App): # <- Main Class
     def build(self):
-        return MyGrid()
+        return kv
 
 
 if __name__ == "__main__":
